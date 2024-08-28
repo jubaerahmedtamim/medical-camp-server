468906816f124c00d8d6d0e6c3355cba70f98c97
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-08-29 03:48:00 +0600

    verifyToken created

--- index.js
+++ index.js
@@ -32,31 +32,62 @@ async function run() {
     const usersCollection = client.db("campDocDB").collection("users");
 
     // jwt related apis
-    app.post('/jwt', async(req, res)=> {
-      const user= req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.send({token});
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token });
     })
 
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access." })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "unauthorized access." })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
     // api for usersCollection.
-    app.get('/users', async(req, res)=> {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result)
     })
-    app.post('/users', async(req, res)=> {
+
+    app.get('/users/admin/:email',verifyToken, async (req, res) => {
+      const email = req.params.email;
+      console.log(req.decoded.email);
+      if (email !== req.decoded?.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin })
+    })
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
       const existingUser = await usersCollection.findOne(query);
-      if(existingUser){
-        return res.send({message: 'user already exists', insertedId: null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async(req, res)=>{
+    app.patch('/users/admin/:id', verifyToken,  async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
           role: 'admin'
@@ -65,48 +96,48 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
-    
+
     // api for campCollection
-    app.post('/camp', async (req, res) => {
+    app.post('/camp', verifyToken, async (req, res) => {
       const campInfo = req.body;
       const result = await campCollection.insertOne(campInfo);
       res.send(result)
     })
-    app.get('/available-camps', async (req, res) => {
+    app.get('/available-camps',  async (req, res) => {
       const result = await campCollection.find().toArray();
       res.send(result)
     })
-    app.get('/manage-camps', async (req, res) => {
+    app.get('/manage-camps', verifyToken, async (req, res) => {
       const addedBy = req.query.addedBy;
       const query = { addedBy: addedBy }
       const result = await campCollection.find(query).toArray();
       res.send(result);
     })
-    app.get('/manage-camp/:id', async (req, res) => {
+    app.get('/manage-camp/:id',verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await campCollection.findOne(query);
       res.send(result);
     })
-    app.delete('/manage-camps/:id', async (req, res) => {
+    app.delete('/manage-camps/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await campCollection.deleteOne(query);
       res.send(result);
     })
-    app.put('/update-camp/:id', async (req, res) => {
+    app.put('/update-camp/:id', verifyToken, async (req, res) => {
       const campInfo = req.body;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          campName:campInfo.campName,
-          campFees:campInfo.campFees,
-          location:campInfo.location,
-          professionalName:campInfo.professionalName,
-          date:campInfo.date,
-          time:campInfo.time,
-          details:campInfo.details,
+          campName: campInfo.campName,
+          campFees: campInfo.campFees,
+          location: campInfo.location,
+          professionalName: campInfo.professionalName,
+          date: campInfo.date,
+          time: campInfo.time,
+          details: campInfo.details,
           image_url: campInfo.image_url,
         }
       }
